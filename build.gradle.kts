f091ddf016cfcf74ff3bef822404ec002128c192
TreeKR <46389044+NamuTree0345@users.noreply.github.com>
2021-03-17 13:33:28 +0900

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ dependencies {
     compileOnly(kotlin("stdlib-jdk8")) //kotlin
     compileOnly("junit:junit:4.12") //junit
     compileOnly("com.destroystokyo.paper:paper-api:1.16.1-R0.1-SNAPSHOT") //paper
-    compileOnly("com.comphenix.protocol:ProtocolLib:4.6.0-SNAPSHOT") //protocollib
+    compileOnly("com.comphenix.protocol:ProtocolLib:4.6.0") //protocollib
     compileOnly("com.github.noonmaru:tap:2.8.8") //tap
     compileOnly("com.sk89q.worldedit:worldedit-bukkit:7.1.0") //worldedit
     implementation("com.github.noonmaru:kommand:0.1.9")
